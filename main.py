648242fe7e384b7d8eec9646b432db2c890bd8f4
a.tyagi <a.tyagi@tu-braunschweig.de>
2025-05-26 22:25:03 +0200

    ingestion and validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mlProject import logger
 from mlProject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<<")
@@ -11,3 +12,14 @@ except Exception as e:
     raise e
 
 logger.info("Welcome to our custom log")
+
+STAGE_NAME = "Data Validation Start"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/mlProject/components/data_ingestion.py
+++ src/mlProject/components/data_ingestion.py
@@ -21,14 +21,14 @@ class DataIngestion:
         else:
             logger.info(f" File already exists of size: {get_size(Path(self.config.local_data_file))}")
 
-    def extract_zip_file(self):
-        """
+"""    def extract_zip_file(self):
+        
         zip_file_path: str
         Extracts the zip file into the data directory
         Function returns None
-        """
+        
         unzip_path = self.config.unzip_dir
         os.makedirs(unzip_path, exist_ok=True)
         with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
-            zip_ref.extractall(unzip_path)
+            zip_ref.extractall(unzip_path)"""
         
